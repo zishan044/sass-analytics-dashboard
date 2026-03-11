9118687e0682d903a5d1efac8de3db958b7c3930
zishan044 <winchesterfelix007@gmail.com>
2026-03-11 07:03:15 +0600

    chore: included tanstack query dev tools as dev dependency

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useState } from "react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
@@ -7,7 +8,10 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>{children}</TooltipProvider>
+      <TooltipProvider>
+        {children}
+        <ReactQueryDevtools />
+      </TooltipProvider>
     </QueryClientProvider>
   );
 }
